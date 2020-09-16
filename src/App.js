ca3335ce2bd3476a8472be058903a28bb04d08bd
moomu2541 <70646986+moomu2541@users.noreply.github.com>
2020-09-16 17:43:16 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.css'; 
-import CharacterCard from './CharacterCard';
+import './App.css';
+import WordCard from './WordCard';
+const word = "Hello";
 function App() {
- return (
- <div>
- <CharacterCard value="h"/>
- <CharacterCard value="i"/>
- </div>
- );
-}export default App;
+    return (
+        <div>
+            <WordCard value="hello 077" />
+        </div>
+    );
+} export default App;
